d4a47f1f4d06065259556d2a94db440acade5f43
Sumit <saam.dhore7@gmail.com>
2024-11-26 15:29:58 +0530

    New 2 Commit

--- Script4.sh
+++ Script4.sh
@@ -5,3 +5,4 @@ Batch=25
 PROFESSION=AWS/DevSecOps/Azure DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
 Hello Myself Sumit Practicinfg Devops
+NEw Changs To Be madde in All Files
